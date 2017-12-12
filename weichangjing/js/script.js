d033b8b8b0251c7721583b5995e3b079344b1b71
dongxiaofei2 <itjy888@163.com>
2017-12-12 15:04:00 +0800

    新增微场景音乐效果

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -14,13 +14,27 @@ var swiper = new Swiper('.swiper-container', {
     },
 
     // 添加动画效果
-    on:{
-        init: function(){
+    on: {
+        init: function () {
             swiperAnimateCache(this); //隐藏动画元素
             swiperAnimate(this); //初始化完成开始动画
         },
-        slideChangeTransitionEnd: function(){
+        slideChangeTransitionEnd: function () {
             swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
         }
     }
 });
+
+//音乐
+var mp3 = document.querySelector("#mp3");
+var mp3btn = document.querySelector("#mp3btn");
+
+mp3btn.addEventListener('click', function () {
+    if (mp3.paused) {
+        mp3.play()
+        mp3btn.style.animationPlayState = 'running';
+    } else {
+        mp3.pause()
+        mp3btn.style.animationPlayState = 'paused';
+    }
+})
